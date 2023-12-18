400f83665bfcb4bc08664b25d0f3b87b3f6283d7
liliningning <2914352881@qq.com>
2023-12-18 11:53:20 +0800

    [修改]将动态数组的APT接口加上动态形参

--- dynamicArray/dynamicArray.h
+++ dynamicArray/dynamicArray.h
@@ -16,30 +16,30 @@ typedef int ELEMENTTYPE; //方便修改
 
 /* API： 接口*/
 //动态数组的初始化
-    int dynamicArrayInit( );
+    int dynamicArrayInit(dynamicArray *pArray );
 
 //动态数组插入数据 （默认在末尾插入数据）
-    int dynamicArrayInsertData();
+    int dynamicArrayInsertData(dynamicArray *pArray,ELEMENTTYPE val );
 
 //动态数组插入数据 （在指定位置插入数据    在插入后面的数据后移）
-    int dynamicArrayAppointPosInsertData();
+    int dynamicArrayAppointPosInsertData(dynamicArray *pArray,int pos,ELEMENTTYPE val);
 
 // 动态数组修改指定位置的数据 
-    int dynamicArrayModifyAppointPosData();
+    int dynamicArrayModifyAppointPosData(dynamicArray *pArray,int pos,ELEMENTTYPE val);
 
 //动态数组删除数据 (默认是删除末尾的数据)
-    int dynamicArrayDeleteData();
+    int dynamicArrayDeleteData(dynamicArray *pArray);
 
 //动态数组删除数据  删除指定位置的数据 后面全部数据前移
-    int dynamicArrayDeleteAppointPosData();
+    int dynamicArrayDeleteAppointPosData(dynamicArray *pArray,int pos);
 
 //动态数组销毁
-    int dynamicArrayDestroy();
+    int dynamicArrayDestroy(dynamicArray *pArray);
 
 //动态数组 获取数组的大小
-    int dynamicArrayGetSize();
+    int dynamicArrayGetSize(dynamicArray *pArray, int *pSize);
 
 //动态数组 获取数组的容量
-    int dynamicArrayGetCapacity();
+    int dynamicArrayGetCapacity(dynamicArray *pArray, int *pCapacity);
 
 #endif
