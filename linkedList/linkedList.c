3f91d33b2b2f35707072dea3eaa7dd5fbe773dbe
liliningning <2914352881@qq.com>
2023-12-20 10:13:35 +0800

    [修改] 完成函数的形参 并在源文件声明

---
+++ linkedList/linkedList.c
@@ -0,0 +1,65 @@
+#include "linkedList.h"
+
+
+
+
+//链表初始化
+    int  LinkListInit(LinkList ** pList)
+    {
+
+
+    }
+
+//链表头插
+    int LinkListHeadInsert(LinkList * pList, ELEMENTTPYE val)
+    {
+
+    }
+
+//链表尾插
+    int LinkListTailInsert(LinkList * pList, ELEMENTTPYE val)
+    {
+
+    }
+
+//链表指定位置插入
+    int LinkListAppointPosInsert(LinkList * pList, int pos, ELEMENTTPYE val)
+    {
+
+    }
+
+//链表的头删
+    int LinkListHeadDel(LinkList * pList)
+    {
+
+    }
+
+//链表的尾删
+    int LinkListTailDel(LinkList * pList)
+    {
+
+    }
+
+//链表的指定位置删除
+    int LinkListAppintPosDel(LinkList * pList, int pos)
+    {
+
+    }
+
+//链表删除指定数据
+    int LinkListDelAppointData(LinkList * pList, ELEMENTTPYE val)
+    {
+
+    }
+
+//获取链表的长度
+    int LinkListGetLength(LinkList * pList, int *pSize)
+    {
+
+    }
+
+// 链表的销毁
+    int LinkListDestroy(LinkList * pList)
+    {
+        
+    }
--- linkedList/main.c
+++ linkedList/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "linkedList.h"
 
 
 int main()
