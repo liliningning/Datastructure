43fe8ddb90c8d4115a30dec74258295b80bb7b3d
liliningning <2914352881@qq.com>
2023-12-20 10:28:38 +0800

    [新增] 新增链表初始化函数的优化

--- linkedList/linkedList.c
+++ linkedList/linkedList.c
@@ -1,11 +1,49 @@
 #include "linkedList.h"
+#include <stdlib.h>
+#include <string.h>
 
 
+// 状态码
+enum STATUS_CODE
+{
+    ON_SUCCESS,
+    NULL_PTR,
+    MALLOC_ERROR,
+    INVALID_ACCESS,
+
+};
 
 
 //链表初始化
     int  LinkListInit(LinkList ** pList)
     {
+        int ret = 0;
+        //链表开辟空间
+        LinkList * list =(ELEMENTTPYE *) malloc(sizeof(LinkList) * 1);
+        if(list == NULL)
+        {
+            return MALLOC_ERROR;
+        }
+        //清楚脏数据 
+        memset(list, 0, sizeof(LinkList) * 1);
+        //头结点开辟空间
+        list->head = (ELEMENTTPYE *) malloc(sizeof(LinkNode) * 1);
+         if(list->head == NULL)
+        {
+            return MALLOC_ERROR;
+        }
+        //清楚脏数据 
+         memset(list->head, 0, sizeof(LinkNode) * 1);
+         list->head->data = 0;
+         list->head->next = NULL;
+
+        //链表长度为0
+        list->len = 0;
+
+        //二级指针
+        *pList = list;
+        return ret;
+
 
 
     }
