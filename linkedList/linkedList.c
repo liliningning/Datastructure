dc6db33823f7b1d16ff595f6187e1f1108a16af5
liliningning <2914352881@qq.com>
2023-12-20 11:15:25 +0800

    [修改] 完成链表的插入和获取链表长度接口

--- linkedList/linkedList.c
+++ linkedList/linkedList.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 // 状态码
 enum STATUS_CODE
 {
@@ -13,97 +12,131 @@ enum STATUS_CODE
 
 };
 
-
-//链表初始化
-    int  LinkListInit(LinkList ** pList)
+// 链表初始化
+int LinkListInit(LinkList **pList)
+{
+    int ret = 0;
+    // 链表开辟空间
+    LinkList *list = (ELEMENTTPYE *)malloc(sizeof(LinkList) * 1);
+    if (list == NULL)
     {
-        int ret = 0;
-        //链表开辟空间
-        LinkList * list =(ELEMENTTPYE *) malloc(sizeof(LinkList) * 1);
-        if(list == NULL)
-        {
-            return MALLOC_ERROR;
-        }
-        //清楚脏数据 
-        memset(list, 0, sizeof(LinkList) * 1);
-        //头结点开辟空间
-        list->head = (ELEMENTTPYE *) malloc(sizeof(LinkNode) * 1);
-         if(list->head == NULL)
-        {
-            return MALLOC_ERROR;
-        }
-        //清楚脏数据 
-         memset(list->head, 0, sizeof(LinkNode) * 1);
-         list->head->data = 0;
-         list->head->next = NULL;
+        return MALLOC_ERROR;
+    }
+    // 清楚脏数据
+    memset(list, 0, sizeof(LinkList) * 1);
+    // 头结点开辟空间
+    list->head = (ELEMENTTPYE *)malloc(sizeof(LinkNode) * 1);
+    if (list->head == NULL)
+    {
+        return MALLOC_ERROR;
+    }
+    // 清楚脏数据
+    memset(list->head, 0, sizeof(LinkNode) * 1);
+    list->head->data = 0;
+    list->head->next = NULL;
 
-        //链表长度为0
-        list->len = 0;
+    // 链表长度为0
+    list->len = 0;
 
-        //二级指针
-        *pList = list;
-        return ret;
+    // 二级指针
+    *pList = list;
+    return ret;
+}
 
+// 链表头插
+int LinkListHeadInsert(LinkList *pList, ELEMENTTPYE val)
+{
+    return reLinkListAppointPosInsert(pList, 0, val);
+}
 
+// 链表尾插
+int LinkListTailInsert(LinkList *pList, ELEMENTTPYE val)
+{
+    return reLinkListAppointPosInsert(pList, len, val);
+}
 
+// 链表指定位置插入
+int LinkListAppointPosInsert(LinkList *pList, int pos, ELEMENTTPYE val)
+{
+    int ret = 0;
+    if (pList == NULL)
+    {
+        return NULL_PTR;
+    }
+    if (pos < 0 || pos > pList->len)
+    {
+        return INVALID_ACCESS;
     }
 
-//链表头插
-    int LinkListHeadInsert(LinkList * pList, ELEMENTTPYE val)
+    // 封装结点
+    LinkNode *newNode = (LinkNode *)malloc(sizeof(LinkNode) * 1);
+    if (newNode == NULL)
     {
-
+        return MALLOC_ERROR;
     }
-
-//链表尾插
-    int LinkListTailInsert(LinkList * pList, ELEMENTTPYE val)
+    memset(newNode, 0, sizeof(LinkNode) * 1);
+    // 赋值
+    newNode->data = val;
+#if 1
+    // 从虚拟头结点开始
+    LinkNode *travelNode = pList->head;
+#else
+    LinkNode *travelNode = pList->head->next;
+#endif
+    while (pos)
     {
-
+        travelNode = travelNode->next;
+        pos--;
     }
+    newNode->next = travelNode->next;
+    travelNode->next = newNode;
+    // 更新链表长度
+    (pList->len)++;
+    return ret;
+}
 
-//链表指定位置插入
-    int LinkListAppointPosInsert(LinkList * pList, int pos, ELEMENTTPYE val)
+// 链表的头删
+int LinkListHeadDel(LinkList *pList)
+{
+}
+
+// 链表的尾删
+int LinkListTailDel(LinkList *pList)
+{
+}
+
+// 链表的指定位置删除
+int LinkListAppintPosDel(LinkList *pList, int pos)
+{
+}
+
+// 链表删除指定数据
+int LinkListDelAppointData(LinkList *pList, ELEMENTTPYE val)
+{
+}
+
+// 获取链表的长度
+int LinkListGetLength(LinkList *pList, int *pSize)
+{
+    int ret = 0;
+    if (pList == NULL)
     {
-
+        return NULL_PTR;
     }
-
-//链表的头删
-    int LinkListHeadDel(LinkList * pList)
+    if (pSize)
     {
-
+        *pSize = pList->len;
     }
-
-//链表的尾删
-    int LinkListTailDel(LinkList * pList)
-    {
-
-    }
-
-//链表的指定位置删除
-    int LinkListAppintPosDel(LinkList * pList, int pos)
-    {
-
-    }
-
-//链表删除指定数据
-    int LinkListDelAppointData(LinkList * pList, ELEMENTTPYE val)
-    {
-
-    }
-
-//获取链表的长度
-    int LinkListGetLength(LinkList * pList, int *pSize)
-    {
-
-    }
+    return ret;
+}
 
 // 链表的销毁
-    int LinkListDestroy(LinkList * pList)
-    {
+int LinkListDestroy(LinkList *pList)
+{
+}
 
-    }
-
-//链表的遍历
-    int linkedListForeach(LinkList * pList)
-    {
-
-    }
+// 链表的遍历
+int linkedListForeach(LinkList *pList)
+{
+    
+}
